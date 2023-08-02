73b4bdc939fb0addeb8eb125c50fdc1cfe8d5969
Alvin Rachmat <80054848+alvinrach@users.noreply.github.com>
2023-08-02 11:24:00 +0700

    Create dartpad.dart

---
+++ dartpad.dart
@@ -0,0 +1,43 @@
+// var gerry = 'alvin';
+// var year  = 1977;
+// var antennaDiameter = 3.7;
+// var image = {
+//   'tags' : ['saturn'],
+//   'url' : '//path/to/saturn.jpg'
+// };
+// // String name = 'Bob';
+// // String name?
+// Object lineCount = 0;
+// // int? lineCount;
+// // assert(lineCount == null);
+// var gerry = 'jajajaa';
+
+// main(){
+//   print('$year');
+//   print('$flybyObjects');
+//   print(lineCount is int);
+//   print(lineCount.runtimeType);
+// }
+
+// var flybyObjects = ['Jupyter', 'Saturnus', 'Uranus', 'Neptune'];
+
+// int
+final lineCount = 5;
+// int lineCount = 5;
+
+void main() {
+final lineCount = 6;
+print(lineCount);
+// print(lineCount.runtimeType);
+if (lineCount==4) {
+  lineCount = 1;
+} else {
+  lineCount = 0;
+}
+print(lineCount);
+}
+
+void maine(){
+  int lineCount = 7;
+  print(lineCount);
+}
